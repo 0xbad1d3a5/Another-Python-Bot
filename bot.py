4f0da83e670c85b6ff65d7fdb42febdab3975f65
0x1bad1d3a <0x1bad1d3a@gmail.com>
2014-04-16 04:44:37 -0700

    fix PMs

--- bot.py
+++ bot.py
@@ -8,14 +8,13 @@ import importlib
 import queue
 import traceback
 
-import sqlite3
-
 import modules
 
 ## This is the class that will communicate with the IRC server ##
 #################################################################
 class irc:
     
+    # Initialize the variables for irc stuff
     # info is a dictionary containing connection information
     def __init__(self, info):
         
@@ -59,11 +58,11 @@ class irc:
         if " :" in msg:
             trailingStart = findn(msg, " :", 1)
             trailing = msg[trailingStart + 2 : ].strip()
-        
-        cmdparam = msg[prefixEnd : trailingStart].strip().split(" ")
-        
+
+        cmdparam = msg[prefixEnd : trailingStart].strip().split(" ")       
         return [prefix, cmdparam[0], cmdparam[1:], trailing]
 
+    # Connect to the server using the information given
     def connect(self, info):
         
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -71,58 +70,56 @@ class irc:
         self.s.send("NICK {}\r\n".format(self.NICK).encode("utf-8"))
         self.s.send("USER {} 0 * :{}\r\n"
                     .format(self.IDENT, self.REALNAME).encode("utf-8"))
-        self.run()
 
     # Send a command to IRC
-    def sendcmd(self, cmd, msg):
-        self.s.send("{} :{}\r\n".format(cmd, msg)
-                    .encode("utf-8", errors="ignore"))
+    def sendcmd(self, cmd, params, msg):
+        if params:
+            params = "".join(params)
+            self.s.send("{} {} :{}\r\n".format(cmd, params, msg)
+                        .encode("utf-8", errors="ignore"))
+        else:
+            self.s.send("{} :{}\r\n".format(cmd, msg)
+                        .encode("utf-8", errors="ignore"))
 
+    # Run the irc connection and return a list of parsed PRIVMSG
     def run(self):
 
         self.buffer = self.buffer + self.s.recv(512).decode("utf-8")
         temp = self.buffer.split("\n")
         self.buffer = temp.pop()
-        
+
+        marray = []
+
         for l in temp:
 
             m = self.parse(l)
- 
+
             # PING - play PING PONG with the server
             if m[1] == "PING":
-                self.sendcmd("PONG", m[3])
+                self.sendcmd("PONG", None, m[3])
 
-            # PRIVMSG - any sort of message
-            elif m[1] == "PRIVMSG":
-                print("{}\n{} from {} - {}".format(m[0], m[1], m[2], m[3]))
-
-            # RPL_LUSERME - good place to join channels
+            # RPL_LUSERME - Use this message to JOIN channels at start
             elif m[1] == "255":
                 channels = self.CHANNELS.split(",")
                 for c in channels:
-                    self.sendcmd("JOIN", c)
-                    
+                    self.sendcmd("JOIN", None, c)
+
+            # INVITE - accept all channel invites automatically
+            elif m[1] == "INVITE":
+                self.sendcmd("JOIN", None, m[3])
+
+            # PRIVMSG - any sort of message
+            elif m[1] == "PRIVMSG":
+                marray.append(m)
+                
             # UNIMPLEMENTED - do nothing for the rest of the commands
             else:
-                print("{} - NEEDS HANDLING\nparams: {}\ntrailing: {}\n"
-                      .format(m[1], m[2], m[3]))
+                print("COMMAND {} - NEEDS HANDLING".format(m[1]))
+                
+        return marray
 
 ## MAIN PROGRAM ##
 ##################
-
-# Import the modules from the modules directory
-modules = [importlib.import_module("." + x, package="modules")
-           for x in modules.__all__]
-
-q = queue.Queue()
-
-for m in modules:
-    mclass = getattr(m, "module")
-    thread = mclass("Hello World!", q)
-    thread.start()
-
-while not q.empty():
-    print(q.get())
 
 # Get settings from info file
 try:
@@ -135,14 +132,31 @@ except IOError:
     print("File Not Found: \"info\"")
     sys.exit(1)
 
-# Create a new irc object
+# Create a new irc instance and connect
 try:
     IRC = irc(info)
+    IRC.connect(info)
 except KeyError:
     print("Error in info file")
     sys.exit(1)
 
-IRC.connect(info)
+# Import the modules from the modules directory
+modules = [importlib.import_module("." + x, package="modules")
+           for x in modules.__all__]
+q = queue.Queue()
 
 while 1:
-    IRC.run()
+    
+    marray = IRC.run()
+
+    for m in marray:
+        for mod in modules:
+            mclass = getattr(mod, "module")
+            if mclass.cmd == m[3][:len(mclass.cmd)]:
+                thread = mclass(m, q)
+                thread.start()
+
+    while not q.empty():
+        m = q.get()
+        print(m)
+        IRC.sendcmd(m[1], m[2], m[3])
--- modules/test_module1.py
+++ modules/test_module1.py
@@ -4,11 +4,11 @@ class module(threading.Thread):
 
     cmd = ".hello"
 
-    def __init__(self, msg, q):
+    def __init__(self, m, q):
         super(module, self).__init__()
-        self.msg = msg
+        self.m = m
         self.q = q
 
     def run(self):
-        self.q.put(self.msg)
+        self.q.put(self.m)
         return
