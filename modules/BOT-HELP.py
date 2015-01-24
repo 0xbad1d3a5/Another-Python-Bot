71a985918eaa05ddf6c8d8356b09f75935a96bcc
0x1bad1d3a <0x1bad1d3a@gmail.com>
2015-01-24 09:20:58 -0500

    edit help

--- modules/BOT-HELP.py
+++ modules/BOT-HELP.py
@@ -8,7 +8,7 @@ class Module(_BaseModule.BaseModule):
         super(Module, self).__init__(msg, share)
 
     def main(self):
-        self.sendpm(".mei [url] [, -lossy] - upload an image to AB's Image Uploader. The -lossy option will produce a jpg at quality 60.")
+        self.sendpm(".mei [url] [, *] - upload an image to AB's Image Uploader. Allows ImageMagick commands.")
         self.sendpm(".random [anime | manga | visual_novels] - return a random anime, manga, or visual novel from the site.")
         self.sendpm(".poll [name | url] - get results of a poll on AB - even if you didn't vote! (set name with .poll [url] [name]).")
         self.sendpm(".pick [arg] [, ...] - pick from a list of choices separated by spaces.")
